da172477294d412fd610af62ad0ede64d43f7578
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2021-09-15 23:47:09 +0430

    Making some changes!

---
+++ Application/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿namespace Application.Controllers
+{
+	public class AccountController : Microsoft.AspNetCore.Mvc.Controller
+	{
+		public AccountController() : base()
+		{
+		}
+
+		public Microsoft.AspNetCore.Mvc.ViewResult Login()
+		{
+			return View();
+		}
+
+		public Microsoft.AspNetCore.Mvc.ViewResult Learn_1000()
+		{
+			return View();
+		}
+
+		public Microsoft.AspNetCore.Mvc.ViewResult Learn_1100()
+		{
+			return View();
+		}
+
+		public Microsoft.AspNetCore.Mvc.ViewResult Learn_1200()
+		{
+			return View();
+		}
+	}
+}
